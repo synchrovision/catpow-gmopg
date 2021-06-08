736393e72423ee776c83833625fedc61545260fb
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-08 13:01:32 +0900

    chore :up: update doc comment

--- catpow-gmopg.php
+++ catpow-gmopg.php
@@ -1,16 +1,15 @@
 <?php
-/**
- * @package Catpow
- * @version 1.0
- */
 /*
 Plugin Name: Catpow GMO Payment Gateway
 Description: Catpow
 Author: synchro_vision
-Version: 1.1
+Version: 1.0.1-alpha
 Author URI: https://catpow.info
 Text Domain: catpow
 Domain Path: /languages
+GitHub Repository: synchrovision/catpow-gmopg
+Requires at least: 5.6
+Requires PHP: 7.4
 */
 
 add_filter('catpow_extensions',function($extensions){$extensions[]=basename(__DIR__);return $extensions;});
